fed5ac4352ede0559166ba11566ea486f01e1972
Akamnex666 <eliisaruiz775@gmail.com>
2025-06-13 19:24:48 -0500

    Agregar configuración de SonarLint para conexión con SonarCloud

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -14,9 +14,18 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
         data-slot="table"
         className={cn("w-full caption-bottom text-sm", className)}
         {...props}
-      />
-    </div>
-  )
+      >
+        <thead>
+          <tr>
+            <th>Columna 1</th>
+            <th>Columna 2</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div> // 
+  );
 }
 
 function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
